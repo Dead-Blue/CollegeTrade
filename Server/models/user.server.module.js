63f5bc9079b5b2cff259c9d1ec980a3b88f204c0
dead-blue <406354451@qq.com>
2015-11-30 10:31:37 +0800

    安装相关依赖

---
+++ Server/models/user.server.module.js
@@ -0,0 +1,93 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+	firstName: String,
+	lastName: String,
+	username: {
+		type:String,
+		trim: true,
+		unique: true,
+		required: true
+	},
+	password: {
+	type: String,
+	validate: [  
+		function(password) {
+			return password && password.length >6;
+		},
+	'Password should be longer'
+	]},
+	salt: {
+		type: String
+	},
+	provider: {
+		type: String,
+		required: 'Provider is required'		
+	},
+	providerId: String,
+	providerData: {},
+	created: {   //定义默认值
+		type: Date,
+		default: Date.now
+	},
+	email: {
+		type: String,
+		match: [/.+\@.+\..+/, "Please fill a valid e-mail address"]   
+	},
+});
+
+UserSchema.virtual('fullName').get(function() {
+	return this.firstName + '' + this.lastName;
+}).set(function(fullName){   
+	var splitName = fullName.split(' ');
+	this.firstName = splitName[0] || '';
+	this.lastName = splitName[1] || '';
+});
+
+
+
+UserSchema.pre('save', function(next) {
+	if(this.password) {
+		this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+		this.password = this.hashPassword(this.password);
+	} 
+	next();
+});
+
+UserSchema.methods.hashPassword = function(password) {
+	return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+};
+
+UserSchema.methods.authenticate = function(password) {
+	return this.password === this.hashPassword(password);
+};
+
+UserSchema.statics.findOneByUsername = function (username, callback) {
+	this.findOne({ username: new RegExp(username, 'i')},callback);
+};
+
+UserSchema.statics.findUniqueUsername = function (username, suffix, callback) {
+	var _this = this;
+	var possibleUsername = username + (suffix || '');
+
+	_this.findOne({
+		username: possibleUsername
+	}, function (err, user) {
+		if (!err) {
+			if (!user) {
+				callback(possibleUsername);
+			} else {
+				return _this.findUniqueUsername(username(suffix || 0) + 1, callback);
+			}
+		} else {
+			callback(null);
+		}
+	});
+};
+UserSchema.set('toJSON', {
+	getters:true, 
+	virtuals:true
+	});
+mongoose.model('User', UserSchema);
