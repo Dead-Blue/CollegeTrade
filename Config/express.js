db73ac2896061e6513d638ab915c0ce02d442cf3
耿夏琛 <DeadBlue@gengxiachendeMacBook-Air.local>
2015-12-01 12:49:28 +0800

    完成基础环境搭建

---
+++ Config/express.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var config = require('./config');
+var morgan = require('morgan'),
+compress = require('compression'),
+bodyParser = require('body-parser'),
+session = require('express-session'),
+methodOverride = require('method-override'),
+passport = require('passport'),
+flash = require('connect-flash'),
+http = require('http'),
+socketio= require('socket.io');
+var MongoStore = require('connect-mongo')({session: session});
+module.exports = function(db){
+	var app =express();
+	var server = http.createServer(app);
+	// var io = socketio.listen(server);
+	if (process.env.NODE_ENV==='development'){
+		app.use(morgan('dev'));
+	} else if (process.env.NODE_ENV==='production') {
+		app.use(compress());
+	}
+	app.use(bodyParser.urlencoded({
+		extended:true
+	}));
+	app.use(bodyParser.json());
+	app.use(methodOverride());
+	
+
+	app.use(session({
+		saveUninitialized: true,
+		resave: true,
+		secret: config.sessionSecret,
+		store: new MongoStore({
+		db: db.connection.db,
+		collection: 'sessions'
+	})
+	}));
+	app.set('views', './Client/views');
+	app.engine('.html', require('ejs').__express);
+	app.set('view engine', 'html');
+	app.use(flash());
+	app.use(passport.initialize());
+	app.use(passport.session());
+	require('../Server/routes/index.js')(app);
+	// require('./socketio')(server,io,mongoStore);
+	app.use(express.static('./Client'));
+	return server;
+}
